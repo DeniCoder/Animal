676ca3b5c87b4bc39be2cc171a675ba205e7031c
DeniCoder <denis2042@yandex.ru>
2025-01-26 16:02:50 +0300

    add Zoo, ZooKeeper, Veterinarian, main
    Добавлены классы зоопарка, смотрителя, ветеринара, а также функция основного кода программы. Правда код с ошибками, надо разбираться дальше

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ class Animal():
         self.name = name
         self.age = age
 
+    def pain (self):
+        print (f"{self.name} не здоров")
 
 class Bird(Animal):
     def __init__ (self, name, age, flight):
@@ -40,8 +42,46 @@ class Reptile(Animal):
 def animal_sound (animal):
     animal.make_sound()
 
-animals = [Bird("Павлин",2, "Летает невысоко"),
-           Mammal("Кошка",1, "Любит пить молоко"), Reptile("Крокодил",5, "Не ядовитый")]
-for animal in animals:
-    animal_sound(animal)
 
+class Zoo():
+    def __init__ (self):
+        self.bird = Bird()
+        self.mammal = Mammal()
+        self.reptile = Reptile()
+        self.zookeeper = ZooKeeper()
+        self.veterinarian = Veterinarian()
+
+    def add_animal (self, animal):
+        self.animals.append(animal)
+
+    def add_zookeeper(self, zookeeper):
+        self.zookeepers.append(zookeeper)
+
+    def add_veterinarian(self, veterinarian):
+        self.veterinarians.append(veterinarian)
+
+class ZooKeeper():
+    def __init__ (self, name):
+        self.name = name
+
+    def feed_animal (self, zoo):
+        for animal in zoo:
+            animal.eat()
+
+class Veterinarian():
+    def __init__ (self, name):
+        self.name = name
+
+    def heal_animal():
+        for animal in zoo:
+            animal.pain()
+            print(f"Ветеринар {self.name} лечит {animal.name}")
+
+
+def main():
+    animals = [Bird("Павлин",2, "Летает невысоко"),
+           Mammal("Кошка",1, "Любит пить молоко"), Reptile("Крокодил",5, "Не ядовитый")]
+    for animal in animals:
+        animal_sound(animal)
+
+main()
